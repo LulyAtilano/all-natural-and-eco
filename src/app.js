10bfcb847cceec28b2058378f30a0242609587a6
latilanoh@gmail.com <latilanoh@gmail.com>
2018-08-09 18:13:42 -0500

    se hace la funcionalidad para llamar a la API  de Mercado Libre y manda la información al componente Products

--- src/app.js
+++ src/app.js
@@ -1,2 +1,29 @@
+import React from 'react';
+import Products from './components/Products';
 
-        
+class MercadoLibreApp extends React.Component {
+    constructor() {
+        super()
+          this.state = {
+            products: [],
+        };
+    }
+          
+    componentDidMount() {
+        fetch('https://api.mercadolibre.com/sites/MLA/search?q=aromaterapia&limit=8')
+        .then(response => {
+          //console.log(results);
+          return response.json();
+        })
+        .then(data => {
+          console.log(data.results);
+          this.setState({products: data.results})
+        })
+    }
+    
+    render() {
+        return this.state.products.map(item => <Products product={item} key={item.id} />)
+    }
+}
+
+export default MercadoLibreApp;
